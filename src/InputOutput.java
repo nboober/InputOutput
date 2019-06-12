17866dbdc043415dde2fc43aea00d7776f700322
nboober <nboober@terpmail.umd.edu>
2019-06-12 13:13:11 -0400

    Updated the output text to be more accurate on what to type and the outcome

--- src/InputOutput.java
+++ src/InputOutput.java
@@ -6,14 +6,14 @@ public class InputOutput {
 
         double num1, num2, num3;
 
-        System.out.println("The First Temperature is... ");
+        System.out.println("The First Decimal Number is... ");
         num1 = scanner.nextDouble();
 
-        System.out.println("The Second Temperature is... " );
+        System.out.println("The Second Decimal Number is... " );
         num2 = scanner.nextDouble();
 
         num3 = (num1 + num2)/2;
 
-        System.out.println("The Average Temperature is " + num3);
+        System.out.println("The Average is " + num3);
     }
 }
